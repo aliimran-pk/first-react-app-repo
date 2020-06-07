a8edc58cec6b8eb7dfaa8be84e195f1ead6b0481
Ali Imran <35903658+aliimran-pk@users.noreply.github.com>
2020-06-07 04:23:08 -0400

    Adding first-reacta-app

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <ul>
+         <li>ALI IMRAN</li>
+         <li>PIAIC50192</li>
+         <li>BootCamp Class 2 Assignment</li>         
+       </ul>
+
         <a
           className="App-link"
           href="https://reactjs.org"
